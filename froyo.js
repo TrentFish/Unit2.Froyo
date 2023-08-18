c6896bd6b34176eaa9d6229cfe33a3bf317471ff
fishyboi@MSI <fishyboi@MSI>
2023-08-18 14:31:09 -0400

    Flavor Town

--- froyo.js
+++ froyo.js
@@ -1,7 +1,20 @@
 const flavorInput = prompt(
     "Please enter your flavors separated by commas",
-    "vanilla, chocolate, etc"
+    "vanilla,chocolate,etc..."
 );
 
 const flavorArr = flavorInput.split(",");
-console.log(flavorArr);
+function getFlavors(){
+    const flavorCount = { };
+    for(let i = 0; i < flavorArr.length; i++){
+        const flavor = flavorArr[i];
+        if(flavorCount[flavor] === undefined){
+            flavorCount[flavor] = 0;
+        }
+        flavorCount[flavor]++;
+    }
+    return flavorCount;
+}
+
+console.log(getFlavors(flavorInput));
+
